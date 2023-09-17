488b953bab051875a27e0f0c75404a930f70f4b2
nuralamsiddque <nurasiddque@gmail.com>
2023-09-17 23:14:56 +0600

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+tailwind.config = {
+    theme: {
+      extend: {
+        colors: {
+          clifford: '#da373d',
+          color_primery: '#F85559',
+          color_secondry: '#D80005',
+        }
+      }
+    }
+  },
+  module.exports = {
+    plugins: [
+      require('daisyui'),
+    ],
+  },
+
+  module.exports = {
+    theme: {
+      extend: {
+        backgroundImage: {
+          'hero-pattern': "url('/assets/img/team.png')"
+        }
+      }
+    }
+  }
